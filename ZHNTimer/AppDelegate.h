f3fc33c86790d0b4217eddb82b263d6293e63d59
z0795 <zhanghn0795@dtdream.com>
2017-05-11 13:57:06 +0800

    Initial Commit

---
+++ ZHNTimer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZHNTimer
+//
+//  Created by zhn on 2017/5/11.
+//  Copyright © 2017年 zhn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZHNTimer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZHNTimer
+//
+//  Created by zhn on 2017/5/11.
+//  Copyright © 2017年 zhn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
